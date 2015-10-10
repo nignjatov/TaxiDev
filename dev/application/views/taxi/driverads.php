aad426e73d19fdb4a8da624a76df7e0444406414
Nemanja Ignjatov <ignjatov90@hotmail.com>
2015-10-10 19:05:49 +0200

    Created modals for ads

--- dev/application/views/taxi/driverads.php
+++ dev/application/views/taxi/driverads.php
@@ -14,13 +14,17 @@
                                 <div class="container-fluid">
                                     <div class="row m-bot20">
                                         <div class="form-group">
-                                            <div class="col-md-9">
-                                            </div>
-                                            <div class="col-md-3">
+                                            <div class="col-md-offset-8 col-md-4">
                                                 <div class="btn-group pull-right">
-                                                    <a class="btn btn-primary" data-toggle="modal" onclick="addNewDriverAds()">
-                                                        Add General Ad <i class="fa fa-plus"></i>
-                                                    </a>
+                                                    <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown"">
+                                                        Add General Ad<i class="fa fa-plus"></i>
+                                                    </button>
+                                                    <ul class="dropdown-menu">
+                                                        <li><a href="#driversWantedPostModal" data-toggle="modal">Drivers Wanted Post</a></li>
+                                                        <li><a href="#taxiAddPostModal" data-toggle="modal">Taxi Add Post</a></li>
+                                                        <li><a href="#WantToDrivePostModal" data-toggle="modal">Want to Drive Post</a></li>
+                                                        <li><a href="#CPLSPostModal" data-toggle="modal">Car/Plate/Lease/Sale Post</a></li>
+                                                    </ul>
                                                 </div>
                                             </div>
                                         </div>
@@ -54,13 +58,13 @@
 
 </section>
 
-<!-- Modal View Start -->
-<div class="modal fade" id="driverAdsDetailModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<!-- Drivers Want Post Modal View Start -->
+<div class="modal fade" id="driversWantedPostModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
-                <h4 class="modal-title">Add Driver Ad</h4>
+                <h4 class="modal-title">Add Drivers Wanted Post</h4>
             </div>
             <form class="form-horizontal" id="driverAdsDetailForm">
                 <div class="modal-body">
@@ -92,7 +96,127 @@
         </div>
     </div>
 </div>
-<!-- Modal View End -->
+<!-- Drivers Want Post Modal View End -->
+
+<!-- Taxi add post Modal View Start -->
+<div class="modal fade" id="taxiAddPostModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title">Add Taxi post</h4>
+            </div>
+            <form class="form-horizontal" id="driverAdsDetailForm">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Shift Start</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control m-bot15" id="driver_shift_start" name="shift_start">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Shift End</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control m-bot15" id="driver_shift_end" name="shift_end">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Comment</label>
+
+                        <div class="col-md-6">
+                            <textarea id="comment" rows="6" class="form-control" name="comment"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer" style="display: block;">
+                    <button type="button" class="btn btn-info" id="driverads_submit_button">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Taxi add post  Modal View End -->
+
+<!-- Want to drive post Modal View Start -->
+<div class="modal fade" id="WantToDrivePostModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title">Add Want to Drive post</h4>
+            </div>
+            <form class="form-horizontal" id="driverAdsDetailForm">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Shift Start</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control m-bot15" id="driver_shift_start" name="shift_start">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Shift End</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control m-bot15" id="driver_shift_end" name="shift_end">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Comment</label>
+
+                        <div class="col-md-6">
+                            <textarea id="comment" rows="6" class="form-control" name="comment"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer" style="display: block;">
+                    <button type="button" class="btn btn-info" id="driverads_submit_button">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Want to drive post Modal View End -->
+
+<!-- CPLS Post Modal View Start -->
+<div class="modal fade" id="CPLSPostModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                <h4 class="modal-title">Add Car/Plate/Lease/Sale post</h4>
+            </div>
+            <form class="form-horizontal" id="driverAdsDetailForm">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Shift Start</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control m-bot15" id="driver_shift_start" name="shift_start">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Shift End</label>
+                        <div class="col-md-6">
+                            <input type="text" class="form-control m-bot15" id="driver_shift_end" name="shift_end">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-md-3">Comment</label>
+
+                        <div class="col-md-6">
+                            <textarea id="comment" rows="6" class="form-control" name="comment"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer" style="display: block;">
+                    <button type="button" class="btn btn-info" id="driverads_submit_button">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- CPLS Post Modal View End -->
 </body>
 <script>
     document.title = '<?php echo config_item("site_title");?>: Driver Wanted Ads\' List';
--- dev/application/views/user/upgradeSubscription.php
+++ dev/application/views/user/upgradeSubscription.php
@@ -3,137 +3,137 @@
 <script type="text/javascript">
     var UserEmail = '<?php echo $userInfo->email_id;?>';
 </script>
-<div class="container">
-    <div class="choose_subs_content" style="margin-top: 90px;">
-        <div class="modal-content">
-            <div class="modal-header">
-<!--                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>-->
-                <h4 class="modal-title">We're sorry, this feature is not available with your current plan</h4>
-            </div>
-            <div class="modal-body package">
-                <p class="title">You current plan is <?php echo '<strong>'.$currentSubscriptionInfo['title'].'</strong> ('.$currentSubscriptionInfo['amount_html'].')';?></p>
-                <div class="free-subscription-border">
-                    <table class="table table-responsive">
-                        <thead>
-                            <tr>
-                                <?php
-                                foreach ($allSubscriptionDetail AS $subscription){
-                                    if($subscription["amount"] == 0){
-                                        if($subscription["id"] == 7){
-                                            echo '<th class="col-md-6 free-subscription-header-operator"><strong>Free subscription</strong></th>';
-                                        } else {
-                                            echo '<th class="col-md-6 free-subscription-header-driver"><strong>Free subscription</strong></th>';
+<section id="main-content">
+<section class="wrapper">
+    <div class="row">
+        <div class="col-sm-12">
+            <section class="panel">
+            <div class="choose_subs_content" style="margin-top: 90px;">
+                <div class="modal-content">
+                    <div class="modal-header">
+        <!--                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>-->
+                        <h4 class="modal-title">We're sorry, this feature is not available with your current plan</h4>
+                    </div>
+                    <div class="modal-body package">
+                        <p class="title">You current plan is <?php echo '<strong>'.$currentSubscriptionInfo['title'].'</strong> ('.$currentSubscriptionInfo['amount_html'].')';?></p>
+                        <div class="free-subscription-border">
+                            <table class="table table-responsive">
+                                <thead>
+                                    <tr>
+                                        <?php
+                                        foreach ($allSubscriptionDetail AS $subscription){
+                                            if($subscription["amount"] == 0){
+                                                if($subscription["id"] == 7){
+                                                    echo '<th class="col-md-6 free-subscription-header-operator"><strong>Free subscription</strong></th>';
+                                                } else {
+                                                    echo '<th class="col-md-6 free-subscription-header-driver"><strong>Free subscription</strong></th>';
+                                                }
+                                            }
                                         }
-                                    }
-                                }
-                                ?>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
+                                        ?>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <?php
+                                            foreach ($allSubscriptionDetail AS $subscription){
+                                                if($subscription["amount"] == 0){
+                                                    echo '<td><h3 class="free-subscription-details">'.$subscription["title"].'</h3></td>';
+                                                }
+                                            }
+                                            ?>
+                                        </tr>
+                                        <tr>
+                                            <?php
+                                            foreach ($allSubscriptionDetail AS $subscription){
+                                                if($subscription["amount"] == 0){
+                                                    echo '<td><h4 class="free-subscription-details">'.$subscription["detail"].'</h4></td>';
+                                                }
+                                            }
+                                            ?>
+                                        </tr>
+                                    </tbody>
+                            </table>
+                        </div>
+                        <div>
+                            <table class="table table-responsive">
+                                <thead>
+                                    <tr>
                                     <?php
-                                    foreach ($allSubscriptionDetail AS $subscription){
-                                        if($subscription["amount"] == 0){
-                                            echo '<td><h3 class="free-subscription-details">'.$subscription["title"].'</h3></td>';
+                                        foreach ($allSubscriptionDetail AS $subscription){
+                                            if($subscription["amount"] > 0){
+                                                $amount = $subscription["amount"] == 0 ? '<strong>Free</strong>' : '<strong>AU$ '.sprintf("%.2f",$subscription["amount"] / 100).'</strong>/month';
+                                                if (strcmp($recommendedSubscriptionID,$subscription["id"]) != 0) {
+                                                    if(strcmp($currentSubscriptionInfo['id'], $subscription["id"]) != 0){
+                                                        echo '<th class="normal-subscription-header">'.$amount.'</th>';
+                                                    } else {
+                                                        echo '<th class="current-subscription-header"><div>CURRENT</div>'.$amount.'</th>';
+                                                    }
+                                                } else {
+                                                    if(strcmp($currentSubscriptionInfo['id'], $subscription["id"]) != 0){
+                                                        echo '<th class="recommended-subscription-header"><div>RECOMMENDED</div>'.$amount.'</th>';
+                                                    } else {
+                                                        echo '<th class="current-subscription-header"><div>CURRENT</div>'.$amount.'</th>';
+                                                    }
+                                                }
+                                            }
                                         }
-                                    }
                                     ?>
                                 </tr>
-                                <tr>
-                                    <?php
-                                    foreach ($allSubscriptionDetail AS $subscription){
-                                        if($subscription["amount"] == 0){
-                                            echo '<td><h4 class="free-subscription-details">'.$subscription["detail"].'</h4></td>';
-                                        }
-                                    }
-                                    ?>
-                                </tr>
-                            </tbody>
-                    </table>
-                </div>
-                <div>
-                    <table class="table table-responsive">
-                        <thead>
-                            <tr>
-                            <?php
-                                foreach ($allSubscriptionDetail AS $subscription){
-                                    if($subscription["amount"] > 0){
-                                        $amount = $subscription["amount"] == 0 ? '<strong>Free</strong>' : '<strong>AU$ '.sprintf("%.2f",$subscription["amount"] / 100).'</strong>/month';
-                                        if (strcmp($recommendedSubscriptionID,$subscription["id"]) != 0) {
-                                            if(strcmp($currentSubscriptionInfo['id'], $subscription["id"]) != 0){
-                                                echo '<th class="normal-subscription-header">'.$amount.'</th>';
-                                            } else {
-                                                echo '<th class="current-subscription-header"><div>CURRENT</div>'.$amount.'</th>';
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <?php
+                                            foreach ($allSubscriptionDetail AS $subscription){
+                                                if($subscription["amount"] > 0){
+                                                    echo '<td class="normal-subscription-title">'.$subscription["title"].'</td>';
+                                                }
                                             }
-                                        } else {
-                                            if(strcmp($currentSubscriptionInfo['id'], $subscription["id"]) != 0){
-                                                echo '<th class="recommended-subscription-header"><div>RECOMMENDED</div>'.$amount.'</th>';
-                                            } else {
-                                                echo '<th class="current-subscription-header"><div>CURRENT</div>'.$amount.'</th>';
+                                        ?>
+                                    <tr>
+                                    <tr>
+                                        <?php
+                                            foreach ($allSubscriptionDetail AS $subscription){
+                                                if($subscription["amount"] > 0){
+                                                    echo '<td>'.$subscription["detail"].'</td>';
+                                                }
                                             }
-                                        }
-                                    }
-                                }
-                            ?>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <?php
-                                    foreach ($allSubscriptionDetail AS $subscription){
-                                        if($subscription["amount"] > 0){
-                                            echo '<td class="normal-subscription-title">'.$subscription["title"].'</td>';
-                                        }
-                                    }
-                                ?>
-                            <tr>
-                            <tr>
-                                <?php
-                                    foreach ($allSubscriptionDetail AS $subscription){
-                                        if($subscription["amount"] > 0){
-                                            echo '<td>'.$subscription["detail"].'</td>';
-                                        }
-                                    }
-                                ?>
-                            <tr>
-                            <tr>
-                                <?php
-                                    foreach ($allSubscriptionDetail AS $subscription){
-                                        if($subscription["amount"] > 0){
-                                            echo '<td class="trial_commercial">30 Days Free Trial!</td>';
-                                        }
-                                    }
-                                ?>
-                            <tr>
-                            <tr>
-                                <?php
-                                    foreach ($allSubscriptionDetail AS $subscription){
-                                        if($subscription["amount"] > 0){
-                                            if(strcmp($currentSubscriptionInfo['id'], $subscription["id"]) != 0){
-                                                echo '<td><div class="centered"><button class="btn btn-default btn_recommendend_subscription" onclick="buySubscription('.$subscription["id"].')">Subscribe</button></div></td>';
-                                            } else {
-                                                echo '<td></td>';
+                                        ?>
+                                    <tr>
+                                    <tr>
+                                        <?php
+                                            foreach ($allSubscriptionDetail AS $subscription){
+                                                if($subscription["amount"] > 0){
+                                                    echo '<td class="trial_commercial">30 Days Free Trial!</td>';
+                                                }
                                             }
-                                        }
-                                    }
-                                ?>
-                            <tr>
-                        </tbody>
-                    </table>
+                                        ?>
+                                    <tr>
+                                    <tr>
+                                        <?php
+                                            foreach ($allSubscriptionDetail AS $subscription){
+                                                if($subscription["amount"] > 0){
+                                                    if(strcmp($currentSubscriptionInfo['id'], $subscription["id"]) != 0){
+                                                        echo '<td><div class="centered"><button class="btn btn-default btn_recommendend_subscription" onclick="buySubscription('.$subscription["id"].')">Subscribe</button></div></td>';
+                                                    } else {
+                                                        echo '<td></td>';
+                                                    }
+                                                }
+                                            }
+                                        ?>
+                                    <tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="modal-footer">
-
-            </div>
+            </section>
+         </div>
         </div>
     </div>
-            <div class="modal-footer">
-
-            </div>
-        </div>
-
-    </div>
-</div>
+</section>
+</section>
 </body>
 <script>
     document.title = '<?php echo config_item("site_title");?>: Update Subscription';
